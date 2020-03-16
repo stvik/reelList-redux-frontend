a27b886e13c647a82f7833bd5ce5e3a452bcff29
stvik <stvik2@gmail.com>
2020-03-16 17:31:20 -0400

    front page links

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { Grid, Typography, Button, ThemeProvider, createMuiTheme } from '@material-ui/core'
 import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
 import TheatersOutlinedIcon from '@material-ui/icons/TheatersOutlined';
@@ -12,8 +13,8 @@ const Homepage = ()  => {
 				<Typography variant='h1' className='HomeHeader' style={{fontSize: '200px', fontWeight: '300'}}> <TheatersOutlinedIcon style={{fontSize:'150px'}} />ReelList </Typography>
 			</Grid>
 			<Grid container direction='row' justify='left' alignItems='flex-end' xs={12}>
-				<Button variant='contained' color='primary'>Search Movies</Button>
-				<Button variant='contained' color='secondary'>View Lists</Button>
+				<Link to="/movies"><Button variant='contained' color='primary'>Search Movies</Button></Link>
+				<Link to="/lists"><Button variant='contained' color='secondary'>View Lists</Button></Link>
 			</Grid>
 	    </Grid>
 
